80365bb76f796d910aa8bc576025146ce282fe3f
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2025-03-07 17:31:58 -0300

    add network besu

--- audita-contract/hardhat.config.ts
+++ audita-contract/hardhat.config.ts
@@ -3,6 +3,12 @@ import { HardhatUserConfig, task } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+  networks: {
+    besu: {
+      url: "http://localhost:8545",
+      accounts: ["0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63"]
+    }
+  }
 };
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
