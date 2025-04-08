39bfc09956582c8102eacb498b97a485c15b985c
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2025-04-08 20:46:59 -0300

    wip

--- audita-worker/src/main.rs
+++ audita-worker/src/main.rs
@@ -9,8 +9,13 @@ use config::AppConfig;
 use tokio::{net, runtime::Builder};
 
 fn main() {
+    env_logger::init();
+
+    log::info!("Starting application");
+
     let config = AppConfig::load("Config.toml");
-    println!("{:#?}", config);
+
+    log::info!("Configuration loaded from Config.toml");
 
     let runtime = Builder::new_multi_thread()
         .worker_threads(config.threads)
@@ -18,23 +23,43 @@ fn main() {
         .build()
         .expect("Failed to build runtime");
 
+    log::info!("Runtime built with {} worker threads", config.threads);
+
     runtime.block_on(server(config));
 }
 
 async fn server(config: AppConfig) {
+    log::info!("Server starting");
+
     let (tx, rx) = channel::new(config.queue_size);
 
+    log::debug!("Channels created with queue size {}", config.queue_size);
+
     tokio::spawn(task::worker(config.clone(), tx.clone(), rx.clone()));
+    log::info!("Spawned worker task");
+
     tokio::spawn(task::ethereum(config.clone(), tx.clone(), rx.clone()));
+    log::info!("Spawned ethereum task");
+
     tokio::spawn(task::elastic(config.clone(), tx.clone(), rx.clone()));
+    log::info!("Spawned elastic task");
 
     let app = route::create_router(tx.clone());
+    log::debug!("Router created");
 
-    let listener = net::TcpListener::bind((config.host.clone(), config.port.clone()))
-        .await
-        .unwrap();
+    let listener = match net::TcpListener::bind((config.host.clone(), config.port.clone())).await {
+        Ok(listener) => listener,
+        Err(err) => {
+            let endpoint = format!("{}:{}", config.host, config.port);
+            log::error!("Failed to bind to {}: {:?}", endpoint, err);
+            return;
+        }
+    };
 
-    if let Err(err) = axum::serve(listener, app).await {
-        eprintln!("Server encountered an error during execution: {err}");
+    log::info!("Server listening on {}:{}", config.host, config.port);
+
+    match axum::serve(listener, app).await {
+        Ok(_) => log::info!("Server terminated gracefully"),
+        Err(err) => log::error!("Server encountered an error during execution: {:?}", err),
     }
 }
