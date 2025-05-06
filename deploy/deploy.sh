4c9ed501a362718abc632640f1199d854c961e9c
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2025-05-06 14:52:57 -0300

    update script to deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker volume remove auditalogs
+docker volume create auditalogs
 docker compose -f besu.yml up -d
 docker compose -f elastic.yml up -d
 docker compose -f app.yml up -d
--- deploy/undeploy.sh
+++ deploy/undeploy.sh
@@ -4,3 +4,4 @@ docker compose -f besu.yml down -v
 docker compose -f elastic.yml down -v
 docker compose -f app.yml down -v
 docker compose -f monitoring.yml down -v
+docker volume remove auditalogs
