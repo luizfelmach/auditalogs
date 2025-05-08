8d9311b6b350c296d37dacb135c631b1bcde1045
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2025-05-08 13:49:53 -0300

    update env

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  envPrefix: "AUDITA_",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
